60554426a7bea128077d5daa6b57ef8ccb8b08ea
Evgenia Kurineva <kurineva85@mail.ru>
2020-09-11 15:10:38 +0300

    Version 5.0. Design section. Video. Icons with ACF.

--- wp-content/themes/bluerex/front-page.php
+++ wp-content/themes/bluerex/front-page.php
@@ -3,185 +3,189 @@
 <?php
 $watch_category = get_category(3);
 if ($watch_category):
-$posts = get_posts(array('numberposts' => 3, 'category' => $watch_category->term_id, 'order' => 'ASC'));
-if ($posts):
-?>
-<section class="section-watch section-tabs <?php echo bluerex_get_background('section_img', $watch_category); ?>">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 mb-5">
-                <?php if (get_field('section_header', $watch_category)): ?>
-                <h3><?php the_field('section_header', $watch_category) ?></h3>
-                <?php endif ?>
-                <?php if ($watch_category->description): ?>
-                <h4><?php echo $watch_category->description ?></h4>
-                <?php endif ?>
-                <ul class="nav nav-pills" id="myTab" role="tablist">
-                    <?php
-                    $data = [];
-                    $i = 0;
-                    foreach ($posts as $post):
-                    setup_postdata($post);
-                    $data[$i]['post_name'] = $post->post_name;
-                    $data[$i]['url'] = get_the_permalink();
-                    $data[$i]['content'] = get_the_content('');
-                    ?>
-                    <li class="nav-item">
-                        <a
-                            class="nav-link <?php if (!$i) echo 'active'; ?> rounded-pill"
-                            id="<?php echo $post->post_name; ?>-tab"
-                            data-toggle="tab"
-                            href="#<?php echo $post->post_name; ?>"
-                            role="tab"
-                            aria-controls="webdesign"
-                            aria-selected="true"
-                            ><?php the_title() ?></a
-                        >
-                    </li>
-                    <?php
-                    $i++;
-                    endforeach;
-                    ?>
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    <?php foreach ($data as $key => $value): ?>
-                    <div
-                        class="tab-pane fade show <?php if (!$key) echo 'active'; ?>"
-                        id="<?php echo $value['post_name']; ?>"
-                        role="tabpanel"
-                        aria-labelledby="<?php echo $value['post_name']; ?>-tab"
-                        >
-                            <?php echo $value['content']; ?>
-                        <p><a href="<?php echo $value['url']; ?>" class="btn btn-pink btn-shadow"><?php echo __('Read more', 'bluerex'); ?></a></p>
+    $posts = get_posts(array('numberposts' => 3, 'category' => $watch_category->term_id, 'order' => 'ASC'));
+    if ($posts):
+        ?>
+        <section class="section-watch section-tabs <?php echo bluerex_get_background('section_img', $watch_category); ?>">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 mb-5">
+                        <?php if (get_field('section_header', $watch_category)): ?>
+                            <h3><?php the_field('section_header', $watch_category) ?></h3>
+                        <?php endif ?>
+                        <?php if ($watch_category->description): ?>
+                            <h4><?php echo $watch_category->description ?></h4>
+                        <?php endif ?>
+                        <ul class="nav nav-pills" id="myTab" role="tablist">
+                            <?php
+                            $data = [];
+                            $i = 0;
+                            foreach ($posts as $post):
+                                setup_postdata($post);
+                                $data[$i]['post_name'] = $post->post_name;
+                                $data[$i]['url'] = get_the_permalink();
+                                $data[$i]['content'] = get_the_content('');
+                                ?>
+                                <li class="nav-item">
+                                    <a
+                                        class="nav-link <?php if (!$i) echo 'active'; ?> rounded-pill"
+                                        id="<?php echo $post->post_name; ?>-tab"
+                                        data-toggle="tab"
+                                        href="#<?php echo $post->post_name; ?>"
+                                        role="tab"
+                                        aria-controls="webdesign"
+                                        aria-selected="true"
+                                        ><?php the_title() ?></a
+                                    >
+                                </li>
+                                <?php
+                                $i++;
+                            endforeach;
+                            ?>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                            <?php foreach ($data as $key => $value): ?>
+                                <div
+                                    class="tab-pane fade show <?php if (!$key) echo 'active'; ?>"
+                                    id="<?php echo $value['post_name']; ?>"
+                                    role="tabpanel"
+                                    aria-labelledby="<?php echo $value['post_name']; ?>-tab"
+                                    >
+                                        <?php echo $value['content']; ?>
+                                    <p><a href="<?php echo $value['url']; ?>" class="btn btn-pink btn-shadow"><?php echo __('Read more', 'bluerex'); ?></a></p>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
                     </div>
-                    <?php endforeach; ?>
+                    <!-- /.col-md-6 -->
+
+                    <div class="col-lg-6 text-center">
+                        <?php if (get_field('section_add_image', $watch_category)): ?>
+                            <img src="<?php the_field('section_add_image', $watch_category) ?>" alt="" />
+                        <?php endif; ?>
+                    </div>
+                    <!-- /.col-md-6 -->
                 </div>
             </div>
-            <!-- /.col-md-6 -->
-
-            <div class="col-lg-6 text-center">
-                <?php if (get_field('section_add_image', $watch_category)): ?>
-                <img src="<?php the_field('section_add_image', $watch_category) ?>" alt="" />
-                <?php endif; ?>
-            </div>
-            <!-- /.col-md-6 -->
-        </div>
-    </div>
-    <?php
-    wp_reset_postdata();
-    unset($data, $posts);
-    ?>
-</section>
-<!-- /.section-watch -->
-<?php endif; //if($posts)    ?>
+            <?php
+            wp_reset_postdata();
+            unset($data, $posts);
+            ?>
+        </section>
+        <!-- /.section-watch -->
+    <?php endif; //if($posts)    ?>
 <?php endif; //if ($watch_category)    ?>
 
 
 <?php
 $posts = get_posts(array('numberposts' => 3, 'category' => 4, 'order' => 'ASC'));
 if ($posts):
-?>  
-<section class="section-progress text-center">
-    <div class="container">
-        <div class="row">
+    ?>  
+    <section class="section-progress text-center">
+        <div class="container">
+            <div class="row">
                 <?php foreach ($posts as $post): ?>
-            <div class="col-md-4 progress-item">
-            <?php echo $post->post_content ?>
+                    <div class="col-md-4 progress-item">
+                        <?php echo $post->post_content ?>
+                    </div>
+                <?php endforeach; ?>
             </div>
-<?php endforeach; ?>
         </div>
-    </div>
-<?php unset($posts) ?>
-</section>
-<!-- /.section-progress -->
+        <?php unset($posts) ?>
+    </section>
+    <!-- /.section-progress -->
 <?php endif; //if($posts) progress  ?>
 
 <?php
 $lets_category = get_category(5);
-if ($lets_category): 
-?>
-<section class="section-lets text-center" <?php echo bluerex_get_background('section_img', $lets_category)?>>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <?php if($lets_category->name): ?>
-                <h3><?php echo $lets_category->name ?></h3>
-                <?php endif; ?>
-                <?php if(get_field('section_header', $lets_category)): ?>
-                <h4><?php the_field('section_header', $lets_category) ?></h4>
-                <?php endif; ?>
-                <?php if($lets_category->description): ?>
-                <p>
-                    <?php echo $lets_category->description ?>
-                </p>
-                <?php endif;?>
-                <p><a href="<?php echo get_category_link(5)?>" class="btn btn-pink btn-shadow"><?php _e('Read more', 'bluerex')?></a></p>
+if ($lets_category):
+    ?>
+    <section class="section-lets text-center" <?php echo bluerex_get_background('section_img', $lets_category) ?>>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php if ($lets_category->name): ?>
+                        <h3><?php echo $lets_category->name ?></h3>
+                    <?php endif; ?>
+                    <?php if (get_field('section_header', $lets_category)): ?>
+                        <h4><?php the_field('section_header', $lets_category) ?></h4>
+                    <?php endif; ?>
+                    <?php if ($lets_category->description): ?>
+                        <p>
+                            <?php echo $lets_category->description ?>
+                        </p>
+                    <?php endif; ?>
+                    <p><a href="<?php echo get_category_link(5) ?>" class="btn btn-pink btn-shadow"><?php _e('Read more', 'bluerex') ?></a></p>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<!-- /.section-lets text-center -->
+    </section>
+    <!-- /.section-lets text-center -->
 <?php endif; //if ($lets_category)     ?>
 
-<section class="section-design">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <h3>We are best and creative agency</h3>
-                <h4>We turn creative ideas into your business</h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                    ipsam, quas, illo laborum molestias nihil dolore nobis quis, quam
-                    reiciendis asperiores. Accusamus consequatur ipsum asperiores
-                    dolore perferendis, tempore ducimus blanditiis.
-                </p>
+<?php
+$design_category = get_category(6);
+if ($design_category):
+    $posts = get_posts(array('numberposts' => 2, 'category' => $design_category->term_id, 'order' => 'ASC'));
+    if ($posts):
+        ?>
+        <section class="section-design">
+            <div class="container">
                 <div class="row">
-                    <div class="col-md-6 mb-3">
-                        <span><i class="far fa-comments"></i></span>
-                        <h2>Graphic Design</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Fugit, amet totam modi optio, a quae.
-                        </p>
-                        <p><a href="#" class="btn btn-pink btn-shadow">Read more</a></p>
+                    <div class="col-lg-6">
+                        <?php if ($design_category->name): ?>
+                            <h3><?php echo $design_category->name ?></h3>
+                        <?php endif; ?>
+                        <?php if (get_field('section_header', $design_category)): ?>
+                            <h4><?php the_field('section_header', $design_category) ?></h4>
+                        <?php endif; ?>
+                        <?php if ($design_category->description): ?>
+                            <p><?php echo $design_category->description ?></p>
+                        <?php endif; ?>
+                        <div class="row">
+                            <?php foreach ($posts as $post):
+                                setup_postdata($post);
+                                ?>
+                                <div class="col-md-6 mb-3">
+                                    <?php if (get_field('icon')): ?>
+                                        <span><?php the_field('icon') ?></span>
+                                    <?php endif; ?>
+                                    <h2><?php the_title() ?></h2>
+            <?php the_content(''); ?>
+                                    <p><a href="<?php the_permalink() ?>" class="btn btn-pink btn-shadow"><?php _e('Read more', 'bluerex') ?></a></p>
+                                </div>
+        <?php endforeach; ?>
+                        </div>
+                        <!-- /.row -->
                     </div>
-                    <!-- /.col-md-6 -->
-                    <div class="col-md-6 mb-3">
-                        <span><i class="fas fa-bullhorn"></i></span>
-                        <h2>Graphic Design</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Fugit, amet totam modi optio, a quae.
-                        </p>
-                        <p><a href="#" class="btn btn-pink btn-shadow">Read more</a></p>
+                    <!-- /.col-lg-6 -->
+                    <div class="col-lg-6">
+        <?php if ($video = get_field('section_video', $design_category)):
+            $video = str_replace('watch?v=','embed/', $video);
+?>
+                        <div class="embed-responsive embed-responsive-16by9 mt-5">
+
+                                        <iframe
+                                            id="videoPlayer"
+                                            class="embed-responsive-item"
+                                            src="<?php echo $video?>?enablejsapi=1&amp;html5=1&amp;"
+                                            allowfullscreen
+                                            ></iframe>
+                                        <div id="videoPlayBtn"></div>
+                                    </div>
+        <?php endif; ?>
                     </div>
-                    <!-- /.col-md-6 -->
-                </div>
-                <!-- /.row -->
-            </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <div class="embed-responsive embed-responsive-16by9 mt-5">
-                  <!-- <iframe
-                    id="videoPlayer"
-                    class="embed-responsive-item"
-                    src="https://www.youtube.com/embed/bpsl7xrTJR4?enablejsapi=1&html5=1&rel=0&&showinfo=0"
-                    allowfullscreen
-                                      ></iframe> -->
-                    <iframe
-                        id="videoPlayer"
-                        class="embed-responsive-item"
-                        src="https://www.youtube.com/embed/ShiqTYGww4E?enablejsapi=1&html5=1&rel=0&&showinfo=0"
-                        allowfullscreen
-                        ></iframe>
-                    <div id="videoPlayBtn"></div>
+                    <!-- /.col-lg-6 -->
                 </div>
             </div>
-            <!-- /.col-lg-6 -->
-        </div>
-    </div>
-</section>
-<!-- /.section-design -->
+             <?php
+            wp_reset_postdata();
+            unset($posts);
+            ?>
+        </section>
+        <!-- /.section-design -->
+    <?php endif; //if($posts)    ?>
+<?php endif; //if ($design_category)     ?>
 
 <section class="section-work section-tabs">
     <div class="container">
